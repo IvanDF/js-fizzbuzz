5263cdce5be4691f8844511ca0dbc20f0703a7d8
Ivan <delfatti.ivan@gmail.com>
2020-10-20 13:32:07 +0200

    Fix

--- js/rule.js
+++ js/rule.js
@@ -13,9 +13,9 @@ for ( i = 0; i < 100; i++ ) {
 
     n++
     
-    if ( (n % 3 == 0) !== ( n % 5 == 0  ) ) {
+    if ( n % 3 == 0 ) {
         console.log('Fizz');
-    } else if ( (n % 5 == 0) !== (n % 3 == 0) )  {
+    } else if ( n % 5 == 0)  {
         console.log('Buzz')
     } else if ( ( n % 3 == 0 ) && ( n % 5 == 0 ) ) {
         console.log('FizzBuzz');
